308a9ae584bb23680daad9cbc548e2d8a6106f02
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-04-20 14:39:40 -0600

    Computes the average score on metrics and averages it

--- heatmap.py
+++ heatmap.py
@@ -1,21 +1,25 @@
 # heatmap.py
 
-import metrics
-import compressions
+from metrics import metrics
+from compressions import random_compression
+from LZ78 import decode
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 def test(CR1, CR2):
-    iters = 1000
+    iters = 10
+    results = np.zeros((len(metrics), iters))
     for i in range(iters):
-            # generate a random compression with CR1
-            # generate a random compression with CR2
-            # evaluate all metrics on this pair of strings
-            # store results
-        # store average result in heatmap
+        decompressed = tuple(map(decode, map(random_compression, (CR1, CR2))))
+        for index, metric in enumerate(metrics):
+            results[index][i] = metrics[metric](decompressed[0],decompressed[1])
+    results = np.mean(results, axis=1)
+    return {a:b for a,b in zip(metrics,results)}
 
+def makeHeatmap(min_limit, max_limit):
+    for i in range(min_limit, max_limit):
+        for j in range(i, max_limit):
+            print("Running {} and {}".format(i, j))
+            print(test(i, j))
 
-def makeHeatmap(min, max):
-    for i in range(min, max):
-        for j in range(i, max):
-            test(i, j)
+makeHeatmap(1, 10)
