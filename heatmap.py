0cc9bf9a9989b251389870e4d3cdcd295f1b179d
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-04-21 20:25:53 -0600

    Producing some graphs now

--- heatmap.py
+++ heatmap.py
@@ -4,7 +4,9 @@ from metrics import metrics
 from compressions import random_compression
 from LZ78 import (decode, encode)
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 import numpy as np
+import pickle
 
 
 def compression_ratio(decompressed):
@@ -13,10 +15,10 @@ def compression_ratio(decompressed):
 
 def test(CRs):
     CR1, CR2 = CRs
-    iters = 1
+    iters = 2
     results = np.zeros((iters, len(metrics)))
     compression_ratios = np.zeros((iters, 2))
-    for i in range(iters):
+    for i in range(max(2, iters)):
         decompressed = tuple(map(decode, map(random_compression, (CR1, CR2))))
         compression_ratios[i][0], compression_ratios[i][1] = tuple(
             map(compression_ratio, decompressed))
@@ -29,14 +31,24 @@ def test(CRs):
     return np.concatenate((compression_ratios, results))
 
 
-def makeHeatmap(min_limit, max_limit):
+def renew(min_limit, max_limit):
     print("Making heatmap with Min {} and Max {}".format(min_limit, max_limit))
-    size = ((max_limit-min_limit)//0.5)+1
     steps = 0.5
     cr_x = np.arange(min_limit, max_limit, steps)
     cr_y = np.arange(min_limit, max_limit, steps)
-    result = map(test, itertools.product(cr_x, cr_y))
-    print(np.array(list(result)))
+    result = np.array(list(map(test, itertools.product(cr_x, cr_y))))
+    result.tofile('memo.dat')
 
 
-makeHeatmap(0.1, 2)
+def makeHeatmap():
+    result = np.fromfile('memo.dat', dtype=float)
+    result = result.reshape((len(result)//5, 5))
+    print(result)
+    # TODO avoid duplicates
+    ax = plt.axes(projection='3d')
+    ax.scatter(result[:, 0], result[:, 1], result[:, 2])
+    plt.show()
+
+
+renew(5, 15)
+# makeHeatmap()
