b28d337c2114aab983a9486fc87afab9021a6e5f
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2020-04-22 17:46:02 -0400

    grep the other string metrics

--- run.sh
+++ run.sh
@@ -7,3 +7,5 @@ python3 -u heatmap.py | tee data/output.txt
 grep "Hamming" data/output.txt >> data/HammingData.txt
 grep "LCS" data/output.txt >> data/LCSData.txt
 grep "Edit" data/output.txt >> data/EditData.txt
+grep "Reconstruction" data/output.txt >> data/ReconstructionData.txt
+grep "Sequence" data/output.txt >> data/SequenceData.txt
