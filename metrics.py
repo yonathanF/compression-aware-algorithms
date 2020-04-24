e475999e4a6d2eb271a0f7964420eadcd4d4322d
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-04-23 20:16:06 -0600

    Merging with origin

--- metrics.py
+++ metrics.py
@@ -1,28 +1,31 @@
 # metrics.py
 
 # https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/
-def LCS(X , Y): 
-    # find the length of the strings 
-    m = len(X) 
-    n = len(Y) 
-  
-    # declaring the array for storing the dp values 
-    L = [[None]*(n+1) for i in range(m+1)] 
-  
+
+
+def LCS(X, Y):
+    # find the length of the strings
+    m = len(X)
+    n = len(Y)
+
+    # declaring the array for storing the dp values
+    L = [[None]*(n+1) for i in range(m+1)]
+
     """Following steps build L[m+1][n+1] in bottom up fashion 
     Note: L[i][j] contains length of LCS of X[0..i-1] 
     and Y[0..j-1]"""
-    for i in range(m+1): 
-        for j in range(n+1): 
-            if i == 0 or j == 0 : 
+    for i in range(m+1):
+        for j in range(n+1):
+            if i == 0 or j == 0:
                 L[i][j] = 0
-            elif X[i-1] == Y[j-1]: 
+            elif X[i-1] == Y[j-1]:
                 L[i][j] = L[i-1][j-1]+1
-            else: 
-                L[i][j] = max(L[i-1][j] , L[i][j-1]) 
-  
-    # L[m][n] contains the length of LCS of X[0..n-1] & Y[0..m-1] 
-    return L[m][n] 
+            else:
+                L[i][j] = max(L[i-1][j], L[i][j-1])
+
+    # L[m][n] contains the length of LCS of X[0..n-1] & Y[0..m-1]
+    return L[m][n]
+
 
 def matrix_pretty_print(matrix):
     """Prints the matrix more nicely """
@@ -59,6 +62,8 @@ def HammingDistance(s1, s2):
     return sum(c1 != c2 for c1, c2 in zip(s1, s2))
 
 # the Needleman-Wunsch algorithm for Sequence Alignment
+
+
 def SequenceAlignment(s1, s2):
     pass
 
@@ -66,10 +71,11 @@ def SequenceAlignment(s1, s2):
 def StringReconstruction(s1, s2):
     pass
 
+
 metrics = {
     "Hamming Distance": HammingDistance,
-    #"StringReconstruction": StringReconstruction,
-    #"SequenceAlignment": SequenceAlignment,
+    # "StringReconstruction": StringReconstruction,
+    # "SequenceAlignment": SequenceAlignment,
     "Edit Distance": EditDistance,
     "LCS Length": LCS,
 }
